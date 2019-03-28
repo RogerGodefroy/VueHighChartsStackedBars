a8e52748cba897b8806c1dd9f79e583c82acb946
Roger Godefroy <rogergodefroy@gmail.com>
2019-03-28 15:19:18 +0100

    Code formatting

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import Vue from "vue";
-import App from "./App.vue";
-import HighchartsVue from "highcharts-vue";
+import Vue from 'vue'
+import App from './App.vue'
+import HighchartsVue from 'highcharts-vue'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-Vue.use(HighchartsVue);
+Vue.use(HighchartsVue)
 
 new Vue({
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
